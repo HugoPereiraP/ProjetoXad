1c889d7ae90eb9821fe8c888aceee41127f15bd5
Hugo <Hugopinhata21@gmail.com>
2026-08-31 22:14:03 -0300

    Ajustando commit

---
+++ ProjetoXadrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tabuleiro;
+
+namespace ProjetoXadrez
+{
+    class Program {
+        public static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine("Posicao : " + p);
+        }
+    }
+}
